df7162a1c5d88d884a0553f4775697e8f619d988
Mohammad Aflah Khan <aflah20082@iiitd.ac.in>
2024-08-08 13:43:27 +0200

    Perplexity Setup Finalized, Runs Started

---
+++ Experiments/Perplexity_Runs/run_perplexity_aya_8b.py
@@ -0,0 +1,37 @@
+import os
+
+os.environ["HF_HOME"] = "/NS/llm-1/nobackup/afkhan/HF_CACHE/Misc"
+os.environ["HF_DATASETS_CACHE"] = "/NS/llm-1/nobackup/afkhan/HF_CACHE/Datasets"
+os.environ["TRANSFORMERS_CACHE"] = "/NS/llm-1/nobackup/afkhan/HF_CACHE/Models"
+
+cache_dir = os.getenv("TRANSFORMERS_CACHE")
+
+import argparse
+
+arg_parser = argparse.ArgumentParser()
+
+arg_parser.add_argument("--cuda_device_id", default="")
+
+args = arg_parser.parse_args()
+
+cuda_device_id = args.cuda_device_id
+
+if cuda_device_id not in ["0", "1"]:
+    raise ValueError("Invalid value for cuda_device_id")
+
+cuda_device_id = int(cuda_device_id)
+
+
+task_prepend = "hellaswag"
+tasks = "ar,bn,ca,da,de,es,eu,fr,gu,hi,hr,hu,hy,id,it,kn,ml,mr,ne,nl,pt,ro,ru,sk,sr,sv,ta,te,uk,vi"
+task_list = list(tasks.split(","))
+TASKS = [task_prepend + "_" + task for task in task_list]
+TASKS.append("hellaswag")
+# add _ppl suffix to all tasks
+TASKS = [task + "_ppl" for task in TASKS]
+
+run_command = f"""lm_eval --model hf --model_args pretrained='/NS/llm-1/nobackup/afkhan/Model_Saves/aya-23-8B' --tasks {','.join(TASKS)} --batch_size auto --write_out --seed 42 --output_path /NS/llm-1/work/afkhan/Perplexity-vs-Evaluation/Experiments/Perplexity_Runs/aya-23-8B-ppl_hellaswag_with_okapi_without_template --log_samples --wandb_args project=PPL_vs_Eval,name=aya-23-8B-ppl_hellaswag_with_okapi_without_template >> /NS/llm-1/work/afkhan/Perplexity-vs-Evaluation/Experiments/Perplexity_Runs/Logs/aya-23-8B-ppl_hellaswag_with_okapi_without_template.log"""
+
+print(run_command)
+
+os.system(run_command)
---
+++ Experiments/Perplexity_Runs/run_perplexity_chat_aya_35b.py
@@ -0,0 +1,21 @@
+import os
+
+os.environ["HF_HOME"] = "/NS/llm-1/nobackup/afkhan/HF_CACHE/Misc"
+os.environ["HF_DATASETS_CACHE"] = "/NS/llm-1/nobackup/afkhan/HF_CACHE/Datasets"
+os.environ["TRANSFORMERS_CACHE"] = "/NS/llm-1/nobackup/afkhan/HF_CACHE/Models"
+
+cache_dir = os.getenv("TRANSFORMERS_CACHE")
+
+task_prepend = "hellaswag"
+tasks = "ar,bn,ca,da,de,es,eu,fr,gu,hi,hr,hu,hy,id,it,kn,ml,mr,ne,nl,pt,ro,ru,sk,sr,sv,ta,te,uk,vi"
+task_list = list(tasks.split(","))
+TASKS = [task_prepend + "_" + task for task in task_list]
+TASKS.append("hellaswag")
+# add _ppl suffix to all tasks
+TASKS = [task + "_chat_ppl" for task in TASKS]
+
+run_command = f"""lm_eval --model hf --model_args pretrained='/NS/llm-1/nobackup/afkhan/Model_Saves/aya-23-35B' --tasks {','.join(TASKS)} --batch_size auto --write_out --seed 42 --output_path /NS/llm-1/work/afkhan/Perplexity-vs-Evaluation/Experiments/Perplexity_Runs/aya_23_35B_chat_ppl_hellaswag_with_okapi_with_template --log_samples --wandb_args project=PPL_vs_Eval,name=aya_23_35B_chat_ppl_hellaswag_with_okapi_with_template >> /NS/llm-1/work/afkhan/Perplexity-vs-Evaluation/Experiments/Perplexity_Runs/Logs/aya_23_35B_chat_ppl_hellaswag_with_okapi_with_template.log"""
+
+print(run_command)
+
+os.system(run_command)
